881d995a2d7d45148a1e05529d86e79d25f4a97d
Samreaksa <rsam@adalte.com>
2020-01-06 16:35:44 +0700

    Initial

---
+++ src/Message.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+        <h1>Testing Vuejs Message!!</h1>
+    </div>
+</template>
---
+++ src/main.js
@@ -0,0 +1,18 @@
+import Vue from 'vue'
+import App from './App.vue'
+import Message from './Message.vue'
+
+// Vue.component('name', {
+//   data() {
+//     return {
+//       property: '',
+//     }
+//   },
+// });
+
+Vue.component('app-message',Message);
+
+new Vue({
+  el: '#app',
+  render: h => h(App)
+})
