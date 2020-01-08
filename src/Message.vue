395b617c1b92f07e5c238aac7dbe3b446707fbab
Samreaksa <rsam@adalte.com>
2020-01-08 12:03:14 +0700

    Added local component.

--- src/Message.vue
+++ src/Message.vue
@@ -1,5 +1,21 @@
 <template>
     <div>
-        <h1>Testing Vuejs Message!!</h1>
+        <h1>{{ message }}</h1>
+        <app-input></app-input>
     </div>
 </template>
+
+<script>
+    import Input from './Input.vue';
+
+    export default {
+        data() {
+            return {
+                message: 'This is a great Message!'
+            }
+        },
+        components: {
+            'app-input': Input
+        }
+    }
+</script>
